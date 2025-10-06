ca709159b3bf67490367c7f2e4ba5d4ede4b6aed
AhmedAbdELMoneim1 <159816505+AhmedAbdELMoneim1@users.noreply.github.com>
2025-10-06 23:54:27 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+pip install --upgrade pip
+pip install numpy==2.3.3 cartopy==0.25.0 dask==2025.9.1 earthaccess==0.15.1 matplotlib==3.10.6 xarray==2025.9.1 earthaccess[virtualizarr] keyring==21.8.0 fastapi==0.115.4 uvicorn==0.32.0 mangum==0.17.0
+pip install arcgis==2.4.1.3 --no-deps --force-reinstall
